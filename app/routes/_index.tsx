3b4bbaaf6deff6a29db466a079fb7118c8cc5abb
Samuel Ogbaje <samuelogbaje@gmail.com>
2025-01-04 21:00:07 +0100

    still working my way through

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,6 +2,7 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
+    { logo: "/favicon.ico" },
     { title: "Werk" },
     { name: "description", content: "Welcome to WERK!" },
   ];
@@ -9,10 +10,16 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div>
-        <h1>Hello jon</h1>
-      </div>
+    <div className="grid p-8">
+      <header className="flex justify-between items-center px-4">
+        <h1>WERK</h1>
+        <ul className="flex  flex-row space-x-4 list-none flex-wrap">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Login</li>
+        </ul>
+      </header>
     </div>
   );
 }
