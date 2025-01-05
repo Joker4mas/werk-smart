8f8e0fa7692b6ff0ad2c5a09fbad03b37965e30d
Samuel Ogbaje <samuelogbaje@gmail.com>
2025-01-05 21:01:02 +0100

    added some assets

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,15 +10,26 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="grid p-8">
-      <header className="flex justify-between items-center px-4">
-        <h1>WERK</h1>
-        <ul className="flex  flex-row space-x-4 list-none flex-wrap">
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Login</li>
+    <div className="grid px-16 ">
+      <header className="flex items-center  flex-wrap flex-1 gap-8 mt-4">
+        <img src="/public/logo.png" alt="" />
+        <ul className="flex space-x-4 list-none flex-wrap flex-1 items-center justify-end">
+          <li>Pricing</li>
+          <li>Product</li>
+          <li>Learn More</li>
         </ul>
+        <div className="login flex gap-4 flex-wrap">
+          <button className="hover:border-2 border-blue-600  w-32 hover:rounded-2xl">
+            Login
+          </button>
+          <button
+            className="w-32 rounded-2xl bg-blue-700
+            hover:border-2 hover:border-blue-600 hover:bg-black
+          "
+          >
+            Get Started
+          </button>
+        </div>
       </header>
     </div>
   );
