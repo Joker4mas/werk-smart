4bb57440a030db68e42bac998b2bff02af6a38a2
Samuel Ogbaje <samuelogbaje@gmail.com>
2025-01-18 10:46:54 +0100

    fixing the right side bar

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -3,29 +3,30 @@ import { GiNotebook } from "react-icons/gi";
 import { AiOutlineMessage } from "react-icons/ai";
 import { SlMap, SlFolderAlt, SlLogout, SlSettings } from "react-icons/sl";
 import { RiRoadMapLine } from "react-icons/ri";
-import { FaFolder } from "react-icons/fa6";
 import {
   PiClipboardTextThin,
-  PiGreaterThan,
+  PiFolderThin,
   PiGreaterThanThin,
 } from "react-icons/pi";
 import { Link } from "@remix-run/react";
-import { FaGreaterThan } from "react-icons/fa";
 function dashboard() {
   return (
     <div className="bg-gray-100 text-black">
       <div className="grid-Container grid grid-flow-col">
         {/* Example: <Sidebar /> */}
-        <div className="side-bar border-2 border-gray-700 w-56 bg-primary p-4 h-screen text-white">
+        <div className="side-bar border-2 border-gray-700 w-60 bg-primary p-4 h-screen text-white">
           <div className="sideBar-logo">
             <img src="/public/logo-side.png" alt="" />
           </div>
 
-          <div className="workSpace-container grid grid-cols-1 gap-2 border-2 border-rose-500 place-items-center">
-            <h2>Workspaces</h2>
-            <ul className="workSpace-list flex flex-row">
+          <div className="workSpace-container grid grid-cols-1 gap-2 place-items-start my-6 text-start">
+            <h2 className="text-start">Workspaces</h2>
+            <ul className="workSpace-list flex flex-row items-center ">
               <li>
-                <Link to="/" className="block px-4 py-2 hover:bg-gray-300">
+                <Link
+                  to="/"
+                  className="block px-4 py-2 hover:bg-gray-300  overflow-hidden"
+                >
                   <img src="/public/Ellipse1.png" alt="Work space logo one" />
                 </Link>
               </li>
@@ -40,23 +41,45 @@ function dashboard() {
                 </Link>
               </li>
               <li>
-                <FaGreaterThan />
-                <PiGreaterThan />
                 <PiGreaterThanThin />
               </li>
             </ul>
           </div>
 
-          <LuLayoutDashboard size={32} />
-          <GiNotebook size={32} />
-          <PiClipboardTextThin size={32} />
-          <RiRoadMapLine size={32} />
-          <FaFolder size={32} />
-          <SlMap size={32} />
-          <SlFolderAlt size={32} />
-          <SlLogout size={32} />
-          <SlSettings size={32} />
-          <AiOutlineMessage size={32} />
+          <div className="dashboard-grid-icons grid grid-flow-row gap-4 place-items-start my-6 border-2 border-green-400">
+            <ul>
+              <li>
+                <LuLayoutDashboard size={25} /> dashboard
+              </li>
+              <li>
+                <GiNotebook size={25} />
+              </li>
+              <li>
+                <PiClipboardTextThin size={25} />
+              </li>
+              <li>
+                <RiRoadMapLine size={25} />
+              </li>
+              <li>
+                <PiFolderThin size={25} />
+              </li>
+              <li>
+                <SlMap size={25} />
+              </li>
+              <li>
+                <SlFolderAlt size={25} />
+              </li>
+              <li>
+                <SlLogout size={25} />
+              </li>
+              <li>
+                <SlSettings size={25} />
+              </li>
+              <li>
+                <AiOutlineMessage size={25} />
+              </li>
+            </ul>
+          </div>
 
           <h2>Dashboard</h2>
         </div>
